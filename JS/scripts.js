f04b7ae8dd614b61bc8640bcc232bebf5c3438ca
ssampson <fiskk@live.com>
2019-11-19 11:32:09 -0500

    Catch up commit

--- JS/scripts.js
+++ JS/scripts.js
@@ -11,3 +11,13 @@ function toggleTable() {
     var lTable = document.getElementById("climbs");
     lTable.style.display = (lTable.style.display === "table") ? "none" : "table";
 }
+
+function RunNoteGenertor(delay, iterations) {
+  let Notes = ('E','F','F#','G','G#','A','A#','B','C','C#','D','D#');
+  let String = ('E-String', 'A-String');
+
+  let SecondBetweenNotes = ;
+  let Iterations = 20;
+
+
+}
